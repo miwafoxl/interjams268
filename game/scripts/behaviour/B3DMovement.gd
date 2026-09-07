264e0ba77d33633ea69d9625ada516bae6f43871
Amanda Severo <miwafoxl@proton.me>
2026-09-06 23:28:39 -0300

    b3dmovement: added gravity and jumping mechanic

--- game/scripts/behaviour/B3DMovement.gd
+++ game/scripts/behaviour/B3DMovement.gd
@@ -1,13 +1,17 @@
 class_name B3DMovement extends Behaviour
 
-@export var speed: float = 600.0
+const JUMP_THRESHOLD: float = 5.0
+
+@export var speed: float = 10.0
 @export var heavyness: float = 0.67
-@export var acceleration: float = 1.0
+@export var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity", 9.806)
 
 var body: CharacterBody3D = null
 var vector: Vector3 = Vector3.ZERO
 
 func init() -> void:
+	speed = flags.get("speed", speed)
+	heavyness = flags.get("heavyness", heavyness)
 	body = actor as CharacterBody3D
 
 func condition_physics(_delta: float) -> bool:
@@ -17,7 +21,13 @@ func action_physics(_delta: float) -> void:
 	var _velocity: Vector3 = lerp(body.velocity, \
 		speed * vector, 1 - clampf(heavyness, 0, 0.98))
 	if not _velocity.distance_to(Vector3.ZERO) < 0.5:
-		body.set_velocity(_velocity)
+		body.set_velocity(Vector3(_velocity.x, body.velocity.y, _velocity.z))
 	else:
-		body.set_velocity(Vector3.ZERO)
+		body.set_velocity(Vector3(0, body.velocity.y, 0))
+	if body.velocity.y < JUMP_THRESHOLD:
+		body.velocity.y += vector.y * speed
 	body.move_and_slide()
+
+func _physics_process(delta: float) -> void:
+	if not body == null and not body.is_on_floor():
+		body.velocity.y -= gravity * delta
