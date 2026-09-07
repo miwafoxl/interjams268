4f94a82f444cad849edf3617a49cbbf85bf17985
Amanda Severo <miwafoxl@proton.me>
2026-09-06 23:30:23 -0300

    binput: add jump property, support unfocusable mouse capture for aiming

--- game/scripts/behaviour/BInput.gd
+++ game/scripts/behaviour/BInput.gd
@@ -1,7 +1,8 @@
 class_name BInput extends Behaviour
 
-var move_vector: Vector2 = Vector2.ZERO
-var aim_vector: Vector2 = Vector2.ZERO
+var move_normal: Vector2 = Vector2.ZERO
+var aim_normal: Vector2 = Vector2.ZERO
 
 var fire: bool = false
 var alt_fire: bool = false
+var jump: bool = false
