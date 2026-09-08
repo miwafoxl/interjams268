97a3a643cce26c74d5d31373082397c8dc2ffa6a
Amanda Severo <miwafoxl@proton.me>
2026-09-08 12:30:46 -0300

    roamingplayui: support showing interaction message when close to an interactable object

--- game/scenes/ui/RoamingPlayUI.gd
+++ game/scenes/ui/RoamingPlayUI.gd
@@ -1,3 +1,11 @@
-extends Control
+class_name RoamPlayUI extends Control
 
 @export var player: GameObject = null
+
+static var can_interact: bool = false
+
+func _process(_delta: float) -> void:
+	if can_interact and not %"HBOX INTERACT".visible:
+		%"HBOX INTERACT".set_visible(true)
+	elif not can_interact and %"HBOX INTERACT".visible:
+		%"HBOX INTERACT".set_visible(false)
