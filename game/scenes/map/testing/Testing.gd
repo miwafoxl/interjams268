663dced4b9dfb26a12ae57262520f2bee1dd0037
Amanda Severo <miwafoxl@proton.me>
2026-09-19 00:49:21 -0300

    testing: rename node references to reflect renames in the scene

--- game/scenes/map/testing/Testing.gd
+++ game/scenes/map/testing/Testing.gd
@@ -1,10 +1,10 @@
 extends Node3D
 
 func _ready() -> void:
-	$ENTITY.add_player_controls()
-	$"Test interaction".behaviour_event.connect(test)
+	$PLAYER.add_player_controls()
+	$"INTER TEST".behaviour_event.connect(test)
 
 func test(event: String) -> void:
 	match event:
 		"test":
-			$ENTITY.give_item("fishgame:trouble")
+			$PLAYER.give_item("fishgame:trouble")
